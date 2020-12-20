ebdd9299fe3d11aa0c6f99a4864753a7829b7532
Hans gpp <hans_selmo@hotmail.com>
2020-12-20 19:01:07 +0100

    single chunk rendering

---
+++ projects/player/src/world.cpp
@@ -0,0 +1,84 @@
+
+#include "world.hpp"
+
+// BinaryKey --------------------------------------------
+
+BinaryKey::BinaryKey(int first, int second) { x = first; y = second; }
+
+void BinaryKey::set(int first, int second) { x = first; y = second; }
+
+bool BinaryKey::operator <( const BinaryKey &rhs ) const
+{
+    if( x < rhs.x) return true;
+    if( x > rhs.x) return false;
+    if( y < rhs.y) return true;
+    if( y > rhs.y) return false;
+    return false;
+}
+
+bool BinaryKey::operator ==( const BinaryKey &rhs ) const
+{
+    if(x == rhs.x && y == rhs.y) return true;
+    return false;
+}
+
+// terrainChunks --------------------------------------------
+
+int terrainChunks::getNumVertex() { return vertexPerSide * vertexPerSide; }
+int terrainChunks::getNumIndices() { return (vertexPerSide-1) * (vertexPerSide-1) * 2 * 3; }
+int terrainChunks::getMaxViewDist() { return maxViewDist; }
+
+terrainChunks::terrainChunks(noiseSet noise, float maxViewDist, float chunkSize, unsigned vertexPerSide)
+{
+    this->noise         = noise;
+    this->maxViewDist   = maxViewDist;
+    this->chunkSize     = chunkSize;
+    this->chunksVisible = std::round(maxViewDist/chunkSize);
+    this->vertexPerSide = vertexPerSide;
+}
+
+terrainChunks::~terrainChunks() { }
+
+void terrainChunks::updateVisibleChunks(glm::vec3 viewerPos)
+{
+    // Viewer coordinates in chunk coordinates (origin at chunk (0, 0))
+    int viewerChunkCoord_X = std::round(viewerPos.x / chunkSize);
+    int viewerChunkCoord_Y = std::round(viewerPos.y / chunkSize);
+    int viewerChunkCoord_Z = std::round(viewerPos.z / chunkSize);
+
+    // Delete chunks out of range
+    typedef std::map<BinaryKey, terrainGenerator> dictionary;
+
+    for(dictionary::const_iterator it = chunkDict.begin(); it != chunkDict.end(); ++it)
+    {
+        BinaryKey key = it->first;
+
+        if(key.x < viewerChunkCoord_X - chunksVisible ||
+           key.x > viewerChunkCoord_X + chunksVisible ||
+           key.y < viewerChunkCoord_Y - chunksVisible ||
+           key.y > viewerChunkCoord_Y + chunksVisible )
+            chunkDict.erase(it);
+    }
+
+    // Save chunks in range
+    terrainGenerator generator;
+
+    for(int yOffset = viewerChunkCoord_Y - chunksVisible; yOffset <= viewerChunkCoord_Y + chunksVisible; yOffset++)
+        for(int xOffset = viewerChunkCoord_X - chunksVisible; xOffset <= viewerChunkCoord_X + chunksVisible; xOffset++)
+        {
+            BinaryKey chunkCoord(xOffset, yOffset);             // chunk name
+
+            if(chunkDict.find(chunkCoord) == chunkDict.end())   // if(chunk doesn't exist in chunkDict)
+            {
+                generator.computeTerrain(noise,
+                                         xOffset * chunkSize,
+                                         yOffset * chunkSize,
+                                         1,
+                                         vertexPerSide,
+                                         vertexPerSide  );
+
+                //chunkDict.insert( {chunkCoord, generator} );    // Doesn't require default constructor. If element already exists, insert does nothing
+                chunkDict[chunkCoord] = generator;                // Requires default constructor.
+            }
+        }
+}
